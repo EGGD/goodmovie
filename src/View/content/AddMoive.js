8b8515c13d67f481bd6bf9158cbdcd0c2d89dbcb
陈隆 <772319837@qq.com>
2017-12-27 23:46:06 +0800

    添加页的按钮样式写好了 还差输入框的样式

--- src/View/content/AddMoive.js
+++ src/View/content/AddMoive.js
@@ -1,79 +1,81 @@
 import React, { Component } from 'react';
 import '../css/AddMoive.css';
 class AddMoive extends Component {
-    constructor(){
+    constructor() {
         super();
-        this.state={
-            submitData:[{
-                id:"Name",
-                name:"片名",
-                value:""
-            },{
-                id:"Name_Tile",
-                name:"类型",
-                value:""
-            },{
-                id:"Director",
-                name:"导演",
-                value:""
-            },{
-                id:"Decsription",
-                name:"简介",
-                value:""
-            },{
-                id:"Date_Time",
-                name:"上映时间",
-                value:""
-            },{
-                id:"Create_Time",
-                name:"添加时间",
-                value:new Date()
-            },{
-                id:"Create_User",
-                name:"创建人",
-                value:"1"
-            },{
-                id:"Is_Delete",
-                name:"是否删除",
-                value:"2"
+        this.state = {
+            submitData: [{
+                id: "Name",
+                name: "片名",
+                value: ""
+            }, {
+                id: "Name_Tile",
+                name: "类型",
+                value: ""
+            }, {
+                id: "Director",
+                name: "导演",
+                value: ""
+            }, {
+                id: "Decsription",
+                name: "简介",
+                value: ""
+            }, {
+                id: "Date_Time",
+                name: "上映时间",
+                value: ""
+            }, {
+                id: "Create_Time",
+                name: "添加时间",
+                value: new Date()
+            }, {
+                id: "Create_User",
+                name: "创建人",
+                value: "1"
+            }, {
+                id: "Is_Delete",
+                name: "是否删除",
+                value: "2"
             }]
         }
-        this.valueChange=this.valueChange.bind(this);
-        this.onSubmit=this.onSubmit.bind(this);
+        this.valueChange = this.valueChange.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
     }
-    valueChange(event){
-        let onsetData=this.state.submitData;
-        onsetData[event.target.alt].value=event.target.value;
+    valueChange(event) {
+        let onsetData = this.state.submitData;
+        onsetData[event.target.alt].value = event.target.value;
         this.setState({
-            submitData:onsetData
+            submitData: onsetData
         })
     }
-    onSubmit(){
-        const datalist=this.state.submitData;
-        let data={}
-        for(let i=0;i<datalist.length;i++){
-            if(datalist[i].value===""){
-                alert(datalist[i].id+"不能为空");
+    onSubmit() {
+        const datalist = this.state.submitData;
+        let data = {}
+        for (let i = 0; i < datalist.length; i++) {
+            if (datalist[i].value === "") {
+                alert(datalist[i].id + "不能为空");
                 return
             }
-            data[datalist[i].id]=datalist[i].value;
+            data[datalist[i].id] = datalist[i].value;
         }
         console.log(data);
     }
     render() {
-        let list=this.state.submitData.map((value,key)=>{
-            return(
+        let list = this.state.submitData.map((value, key) => {
+            return (
                 <div className="itemDiv" key={value.id}>
                     <label >{value.name}:</label>
-                    <input type="text" alt={key} value={value.value} onChange={this.valueChange}/>
+                    <input type="text" alt={key} value={value.value} onChange={this.valueChange} />
                 </div>
             )
         })
         return (
-            <div>
-                
+            <div className="addFromDiv">
                 {list}
-                <button onClick={this.onSubmit}>提交</button>
+                <div className="buttonDiv">
+                    <button onClick={this.onSubmit}>提交</button>
+                    <button>重置</button>
+                </div>
             </div>
         )
     }
