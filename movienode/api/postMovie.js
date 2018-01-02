4528849b29b82c4a1cf373d9b54eb5b9499bf590
DESKTOP-NEDMMDF\CL <772319837@qq.com>
2018-01-02 15:13:50 +0800

    单条数据插入写好了 接下来写多图片和多下载地址的sql

--- movienode/api/postMovie.js
+++ movienode/api/postMovie.js
@@ -4,6 +4,7 @@ var mysql = require('mysql');
 var dbmovieConfig = require('../db/DBmovieConfig');
 var AddMovie = require('../db/AddMovie');
 var pool = mysql.createPool(dbmovieConfig.mysql);
+var async = require('async');
 // 响应一个JSON数据
 var responseJSON = function (res, ret) {
     if (typeof ret === 'undefined') {
@@ -16,14 +17,42 @@ var responseJSON = function (res, ret) {
 };
 
 router.post('/',function (req, res, next) {
-    debugger
-    var param = req.query || req.params;
-    // pool.getConnection(function (err, connection) {
-    //   responseJSON(res, "movieData");
-    //   connection.release();
-    // })
-    console.log(req.body);
-    res.send(req.body);
+    var param =  req.body;
+    pool.getConnection(function (err, connection) {
+        async.waterfall([
+            function (callback) { //第一个请求           
+                connection.query(AddMovie.postAddMovie(param), function (err, result) {               
+                    if (result) {
+                        callback(err, result);
+                    }else{
+                        res.send(err);
+                    };
+                })
+            },function (AddMoive, callback) { //第二个请求
+                debugger
+                connection.query(AddMovie.postAddMovieImage(AddMoive.insertId,param.Image_Url), function (err, result) {
+                    if (result) {
+                        callback(err, AddMoive, result);//请求结果返回到下一个请求
+                    }else{
+                        res.send(err);
+                    };
+                })
+            },function (AddMoive, imageData, callback) {//第三个请求
+                connection.query(AddMovie.postAddMovieDownload(AddMoive.insertId,param.Sownload_Url), function (err, result) {
+                    if (result) {
+                        callback(err, AddMoive, imageData, result);//请求结果返回到下一个请求
+                    };
+                })
+            }
+        ], function (err, AddMoive, imageData, downloadData) {//获取前三个请求的结果
+            AddMoive.Image_Url = imageData;
+            AddMoive.Download = downloadData;
+            responseJSON(res, AddMoive);
+            connection.release();
+        })
+    })
+    // console.log(req.body);
+    // res.send(req.body);
 });
 
 //导出
--- movienode/db/AddMovie.js
+++ movienode/db/AddMovie.js
@@ -1,7 +1,15 @@
-function postAddMovie(is_Delete){
-  return `SELECT * FROM movie where Is_Delete=${is_Delete}`
+function postAddMovie(data){
+  return `INSERT INTO movie (Name,Name_Title,Category,Director,Decsription,Date_Time,Create_Time,Create_User,Is_Delete)  VALUES('${data.Name}','${data.Name_Title}','${data.Category}','${data.Director}','${data.Decsription}','${data.Date_Time}','${data.Create_Time}',${data.Create_User},${data.Is_Delete})`
+}
+function postAddMovieImage(id,data){
+  return `INSERT INTO image (Image_Url,Movie_Id) VALUES('${data}',${id})`;
+}
+function postAddMovieDownload(id,data){
+  return `INSERT INTO download (Sownload_Url,Movie_Id) VALUES('${data}',${id})`;
 }
 var AddMovie={
   postAddMovie:postAddMovie,
+  postAddMovieImage:postAddMovieImage,
+  postAddMovieDownload:postAddMovieDownload
 }
 module.exports = AddMovie;
