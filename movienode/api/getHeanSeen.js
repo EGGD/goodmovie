066bf20934cfda11617e6f64326e151b1c368c6c
DESKTOP-NEDMMDF\CL <772319837@qq.com>
2018-01-03 10:46:41 +0800

    上传数据 有多张图片 和多个下载地址 获取数据 有多张图片和多个下载地址 完成。 接下来写多个数据的展示及图片

--- movienode/api/getHeanSeen.js
+++ movienode/api/getHeanSeen.js
@@ -21,6 +21,7 @@ var responseJSON = function (res, ret) {
 //请求 http://localhost:3001/getHeanSeen?is_Delete=2
 router.get('/', function (req, res, next) {
     var param = req.query || req.params;
+    var imglist = [], downloadlist = [];
     pool.getConnection(function (err, connection) {
         async.waterfall([
             function (callback) { //第一个请求           
@@ -30,21 +31,25 @@ router.get('/', function (req, res, next) {
                     };
                 })
             }, function (movieData, callback) { //第二个请求
-                connection.query(HeanSeen.getHeanSeenImage(movieData[0].ID), function (err, result) {
-                    if (result) {
-                        callback(err, movieData, result);//请求结果返回到下一个请求
-                    };
-                })
+                for (let i = 0; i < movieData.length; i++) {
+                    connection.query(HeanSeen.getHeanSeenImage(movieData[i].ID), function (err, result) {
+                        if (result) {
+                            movieData[i].Image_Url=result;
+                            if(i==movieData.length-1)callback(err, movieData, result);//请求结果返回到下一个请求
+                        };
+                    })
+                }
             }, function (movieData, imageData, callback) {//第三个请求
-                connection.query(HeanSeen.getHeanSeenDownload(movieData[0].ID), function (err, result) {
-                    if (result) {
-                        callback(err, movieData, imageData, result);//请求结果返回到下一个请求
-                    };
-                })
+                for (let i = 0; i < movieData.length; i++) {
+                    connection.query(HeanSeen.getHeanSeenDownload(movieData[i].ID), function (err, result) {
+                        if (result) {
+                            movieData[i].Download=result;
+                            if(i==movieData.length-1)callback(err, movieData, imageData, result);//请求结果返回到下一个请求
+                        };
+                    })
+                }
             }
         ], function (err, movieData, imageData, downloadData) {//获取前三个请求的结果
-            movieData[0].Image_Url = imageData;
-            movieData[0].Download = downloadData;
             responseJSON(res, movieData);
             connection.release();
         })
--- movienode/api/postMovie.js
+++ movienode/api/postMovie.js
@@ -16,43 +16,51 @@ var responseJSON = function (res, ret) {
     }
 };
 
-router.post('/',function (req, res, next) {
-    var param =  req.body;
+router.post('/', function (req, res, next) {
+    var param = req.body;
     pool.getConnection(function (err, connection) {
         async.waterfall([
             function (callback) { //第一个请求           
-                connection.query(AddMovie.postAddMovie(param), function (err, result) {               
+                connection.query(AddMovie.postAddMovie(param), function (err, result) {
                     if (result) {
                         callback(err, result);
-                    }else{
+                    } else {
                         res.send(err);
                     };
                 })
-            },function (AddMoive, callback) { //第二个请求
-                debugger
-                connection.query(AddMovie.postAddMovieImage(AddMoive.insertId,param.Image_Url), function (err, result) {
-                    if (result) {
-                        callback(err, AddMoive, result);//请求结果返回到下一个请求
-                    }else{
-                        res.send(err);
-                    };
-                })
-            },function (AddMoive, imageData, callback) {//第三个请求
-                connection.query(AddMovie.postAddMovieDownload(AddMoive.insertId,param.Sownload_Url), function (err, result) {
-                    if (result) {
-                        callback(err, AddMoive, imageData, result);//请求结果返回到下一个请求
-                    };
-                })
+            }, function (AddMoive, callback) { //第二个请求
+                let onimglist = param.Image_Url.split("|");
+                for (let i = 0; i < onimglist.length; i++) {
+                    if (onimglist[i] != "") {
+                        connection.query(AddMovie.postAddMovieImage(AddMoive.insertId, onimglist[i]), function (err, result) {
+                            if (result) {
+                                if(i==onimglist.length-1)callback(err, AddMoive, result);//请求结果返回到下一个请求
+                            } else {
+                                res.send(err);
+                            };
+                        })
+                    }
+                }
+
+            }, function (AddMoive, imageData, callback) {//第三个请求
+                let ondwnlist = param.Sownload_Url.split("|");
+                for (let i = 0; i < ondwnlist.length; i++) {
+                    if (ondwnlist[i] != "") {
+                        connection.query(AddMovie.postAddMovieDownload(AddMoive.insertId, ondwnlist[i]), function (err, result) {
+                            if (result) {
+                                if(i==ondwnlist.length-1)callback(err, AddMoive, imageData, result);//请求结果返回到下一个请求
+                            };
+                        })
+                    }
+                }
             }
         ], function (err, AddMoive, imageData, downloadData) {//获取前三个请求的结果
-            AddMoive.Image_Url = imageData;
-            AddMoive.Download = downloadData;
+            // AddMoive.Image_Url = imageData;
+            // AddMoive.Download = downloadData;
             responseJSON(res, AddMoive);
             connection.release();
         })
     })
-    // console.log(req.body);
-    // res.send(req.body);
 });
 
 //导出
